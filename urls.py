e18828e876598ddacf236ac9da6d824ff055b4f9
eungyu <eungyu@powerspace-lm-2.local>
2009-09-18 22:55:42 -0700

    added loginuser

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from pisces.login.views import loginuser
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
-
+    ('^login/$', loginuser),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': 'static'}),
 )
